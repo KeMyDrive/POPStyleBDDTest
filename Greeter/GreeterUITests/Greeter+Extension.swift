d9b6cfd87fedcebba214d6bce396ec29c27dda15
Ke <ke.ma@mydrivesolutions.com>
2018-07-03 11:51:13 +0100

    complete step 4

--- Greeter/GreeterUITests/Greeter+Extension.swift
+++ Greeter/GreeterUITests/Greeter+Extension.swift
@@ -10,18 +10,18 @@ import XCTest
 
 extension Greetable {
     func givenTheAppIsLaunched() {
-        
+        XCUIApplication().launch()
     }
     
     func thenIShouldSeeGreetButton() {
-        
+        XCTAssertTrue(XCUIApplication().buttons["Greet"].exists)
     }
     
     func whenITapGreetButton() {
-        
+        XCUIApplication().buttons["Greet"].tap()
     }
     
     func thenIShouldSeeWelcomeMessage() {
-        
+        XCTAssertTrue(XCUIApplication().staticTexts["Welcome"].exists)
     }
 }

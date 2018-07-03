846685dd6c6198197a8269fa733c1b70bba9cf75
Ke <ke.ma@mydrivesolutions.com>
2018-07-03 11:14:11 +0100

    add all possible scenarios

---
+++ Greeter/GreeterUITests/Greeter+Protocol.swift
@@ -0,0 +1,12 @@
+//
+//  Greeter+Protocol.swift
+//  GreeterUITests
+//
+//  Created by Ke Ma on 03/07/2018.
+//  Copyright © 2018 MyDrive. All rights reserved.
+//
+
+protocol Greetable {
+    func testHomeScreenHasGreetButton()
+    func testUserShouldGetWelcomeMessageOnceTappedGreetButton()
+}
---
+++ Greeter/GreeterUITests/GreeterUITests.swift
@@ -0,0 +1,25 @@
+//
+//  GreeterUITests.swift
+//  GreeterUITests
+//
+//  Created by Ke Ma on 03/07/2018.
+//  Copyright © 2018 MyDrive. All rights reserved.
+//
+
+import XCTest
+
+class GreeterUITests: XCTestCase {
+        
+    override func setUp() {
+        super.setUp()
+        
+        continueAfterFailure = false
+        XCUIApplication().launch()
+
+    }
+    
+    override func tearDown() {
+        super.tearDown()
+    }
+    
+}

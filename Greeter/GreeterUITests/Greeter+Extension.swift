92e93250af5ec25beda4f0917dc67695815349a4
Ke <ke.ma@mydrivesolutions.com>
2018-07-03 11:36:01 +0100

    add Greeter extension for step definitions

---
+++ Greeter/GreeterUITests/Greeter+Extension.swift
@@ -0,0 +1,27 @@
+//
+//  Greeter+Extension.swift
+//  GreeterUITests
+//
+//  Created by Ke Ma on 03/07/2018.
+//  Copyright © 2018 MyDrive. All rights reserved.
+//
+
+import XCTest
+
+extension Greetable {
+    func givenTheAppIsLaunched() {
+        
+    }
+    
+    func thenIShouldSeeGreetButton() {
+        
+    }
+    
+    func whenITapGreetButton() {
+        
+    }
+    
+    func thenIShouldSeeWelcomeMessage() {
+        
+    }
+}
